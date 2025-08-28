9df28c420d6dbbcbffc16a9cdf48092e35d10fb2
Alexandre Ferreira <112132087+Alexandreferr4@users.noreply.github.com>
2025-08-28 15:39:41 -0300

    Update interacoes.js
    Ajustes botão topo

--- SRC/JS/interacoes.js
+++ SRC/JS/interacoes.js
@@ -3,19 +3,19 @@ btnTopo.id = "btnTopo";
 btnTopo.textContent = "↑";
 btnTopo.style.display = "none";
 btnTopo.style.position = "fixed";
-btnTopo.style.bottom = "20px";
-btnTopo.style.right = "20px";
-btnTopo.style.width = "60px";
-btnTopo.style.height = "60px";
+btnTopo.style.bottom = "30px";
+btnTopo.style.right = "25px";
+btnTopo.style.width = "45px";
+btnTopo.style.height = "45px";
 btnTopo.style.border = "none";
 btnTopo.style.borderRadius = "50%"; // totalmente redondo
 btnTopo.style.backgroundColor = "#0099ff";
-btnTopo.style.color = "#fff";
+btnTopo.style.color = "#ffffffff";
 btnTopo.style.fontSize = "24px";
 btnTopo.style.fontWeight = "bold";
 btnTopo.style.cursor = "pointer";
 btnTopo.style.zIndex = "999";
-btnTopo.style.boxShadow = "0 4px 8px rgba(0,0,0,0.3)";
+btnTopo.style.boxShadow = "0 8px 8px rgba(0, 0, 0, 0.584)";
 document.body.appendChild(btnTopo);
 
 window.onscroll = function () {
